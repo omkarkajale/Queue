aed408dc45bce0af2e5c666c83c281900e9091e0
omkarkajale <115961814+omkarkajale@users.noreply.github.com>
2022-12-26 14:31:43 +0530

    Add files via upload
    The programe gives the area of circle

---
+++ area of circle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int radius ;
+	float pi = 3.144;
+	printf("Enter the radius");
+	scanf("%d",&radius);
+	
+	printf("The area of the circle is = %f",pi*radius*radius);
+	
+}
